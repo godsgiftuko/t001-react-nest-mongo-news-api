887fb31118147a447d108e6fc4231d51a41665ff
God'sgift Uko <godsgiftuko@gmail.com>
2025-03-20 17:49:10 +0100

    Implement cors

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Configs } from './configs';
 import { ValidationPipe } from '@nestjs/common';
+import cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    cors({
+      origin: 'https://t001-react-nest-mongo-news-app.vercel.app', // Replace with your actual Vercel domain
+    }),
+  );
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
   const PORT = Configs.SERVER_PORT;
